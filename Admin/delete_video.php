741c51b499bb4eebb56a1202f8814ef297192d6a
Asadullah <88024587+Asadullah-nadeem@users.noreply.github.com>
2023-06-09 11:52:13 +0530

    Update delete_video.php

--- Admin/delete_video.php
+++ Admin/delete_video.php
@@ -4,13 +4,28 @@ require_once 'conn.php';
 if (isset($_POST['delete'])) {
     $video_id = $_POST['video_id'];
 
-    // Perform the deletion of the video entry in the database
-    mysqli_query($conn, "DELETE FROM `video` WHERE `video_id` = '$video_id'") or die(mysqli_error());
+    // Prepare the delete statement
+    $stmt = mysqli_prepare($conn, "DELETE FROM `video` WHERE `video_id` = ?");
+    mysqli_stmt_bind_param($stmt, "s", $video_id);
 
-    // Additional actions after deletion, such as deleting the corresponding file from the server
+    // Execute the delete statement
+    mysqli_stmt_execute($stmt);
 
-    // Redirect to the index.php page or any other desired location
-    header("Location: index.php");
-    exit();
+    // Check if any rows were affected
+    $affected_rows = mysqli_stmt_affected_rows($stmt);
+    if ($affected_rows > 0) {
+        // Deletion was successful
+        echo "Record deleted successfully.";
+    } else {
+        // No rows were deleted
+        echo "No record found for deletion.";
+    }
+
+    // Close the statement
+    mysqli_stmt_close($stmt);
 }
+
+// Redirect to the index.php page or any other desired location
+header("Location: index.php");
+exit();
 ?>
